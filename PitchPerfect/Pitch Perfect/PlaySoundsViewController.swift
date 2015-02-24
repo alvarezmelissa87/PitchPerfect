fc92e66abe0af6651a883c087179ed97bd95c50a
Melissa Alvarez <melissa@telmate.com>
2015-02-23 19:28:06 -0800

    Add audiofile and play at slow and fast rate

--- PitchPerfect/Pitch Perfect/PlaySoundsViewController.swift
+++ PitchPerfect/Pitch Perfect/PlaySoundsViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
+    
+    var audioPlayer:AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+            var filePathUrl = NSURL.fileURLWithPath(filePath)
+            audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
+            audioPlayer.enableRate = true
+        }else {
+            println("The filepath is empty")
+        }
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +31,23 @@ class PlaySoundsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setUpAudioPlayer(rate:Float) {
+        audioPlayer.stop()
+        audioPlayer.rate = rate
+        audioPlayer.currentTime = 0.0
+        audioPlayer.play()
     }
-    */
+    
+    @IBAction func playSlow(sender: UIButton) {
+        setUpAudioPlayer(0.5)
+    }
+
+    @IBAction func playFast(sender: UIButton) {
+        setUpAudioPlayer(2.0)
+    }
+
+    @IBAction func stopPlayback(sender: UIButton) {
+        audioPlayer.stop()
+    }
 
 }
